635df489605765e67e84c4bf408d06fcd854e0b7
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-10-06 14:46:20 -0300

    Salvando para garantir rsrsrs

---
+++ front_conversor/js/script.js
@@ -0,0 +1,40 @@
+document.getElementById('formulario').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const inicio = document.getElementById('inicio').value;
+    const fim = document.getElementById('fim').value;
+
+    let resultado = 0;
+    let erro = 0;
+
+    document.getElementById('resultado').textContent ="";
+    document.getElementById('erro').textContent="";
+
+    try {
+        const response = await fetch ('http://localhost:8080/converter', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                num1,
+                inicio, 
+                fim
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro na requisição');
+        }
+
+        const data = await response.json();
+
+        if (data.erro) {
+             document.getElementById('erro').textContent = data.erro;
+        } else {
+            document.getElementById('resultado').textContent = 'Resultado: ' + data.resultado;
+        }
+    } catch (err) {
+        document.getElementById('erro').textContent = 'Erro: ' + err.message;
+    }
+});
